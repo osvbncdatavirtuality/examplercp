47cdeae3ef047fa53120db0c89ffac411747b16f
osvbncdatavirtuality <osvaldo.bianchini@datavirtuality.de>
2015-05-29 19:23:41 +0200

    first commit

---
+++ com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/TodoDetailsPart.java
@@ -0,0 +1,15 @@
+package com.example.e4.rcp.todo.parts;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class TodoDetailsPart {
+
+	@PostConstruct
+	public void createControls(Composite parent) {
+	  System.out.println(this.getClass().getSimpleName()
+	  + " @PostConstruct method called.");
+	} 
+
+}
